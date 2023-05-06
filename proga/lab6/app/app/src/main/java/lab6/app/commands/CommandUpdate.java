5af2005d5f7669b19cf02b382450f967262757b2
notttk <den.bilobram@mail.ru>
2023-05-06 15:53:58 +0300

    lab6 fix

--- proga/lab6/app/app/src/main/java/lab6/app/commands/CommandUpdate.java
+++ proga/lab6/app/app/src/main/java/lab6/app/commands/CommandUpdate.java
@@ -1,5 +1,6 @@
 package lab6.app.commands;
 
+import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 import lab6.app.labwork.LabWork;
 import lab6.app.signals.Signal;
@@ -22,22 +23,20 @@ public class CommandUpdate extends Command {
             
             LabWork labWorkUpdate = priorityQueue.stream().filter(x -> x.getId().equals(Long.parseLong(getId()))).findFirst().get();
 
-            if (labWorkUpdate != null) {
-                labWork.setName(labWorkUpdate.getName());
-                labWork.setCoordinates(labWorkUpdate.getCoordinates());
-                labWork.setDifficulty(labWorkUpdate.getDifficulty());
-                labWork.setMinimalPoint(labWorkUpdate.getMinimalPoint());
-                labWork.setTunedInWorks(labWorkUpdate.getTunedInWorks());
-                labWork.setAuthor(labWorkUpdate.getAuthor());
-                resultSignal.setMessage("Элемент был изменём.");
-                resultSignal.setSucces(true);
-            } else {
-                resultSignal.setMessage("Элемент с таким id не найден.");
-                resultSignal.setSucces(false);
-            }
+            labWork.setName(labWorkUpdate.getName());
+            labWork.setCoordinates(labWorkUpdate.getCoordinates());
+            labWork.setDifficulty(labWorkUpdate.getDifficulty());
+            labWork.setMinimalPoint(labWorkUpdate.getMinimalPoint());
+            labWork.setTunedInWorks(labWorkUpdate.getTunedInWorks());
+            labWork.setAuthor(labWorkUpdate.getAuthor());
+            resultSignal.setMessage("Элемент был изменём.");
+            resultSignal.setSucces(true);
 
         } catch (NumberFormatException exp) {
             resultSignal.setMessage("Неверный формат аргумента id.");
+            resultSignal.setSucces(false);
+        } catch (NoSuchElementException exp) {
+            resultSignal.setMessage("Элемент с таким id не найден.");
             resultSignal.setSucces(false);
         }
         
