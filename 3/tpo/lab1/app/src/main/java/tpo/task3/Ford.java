cefeadb2fd267c611a6e9b64c14e505d0f01b771
notttk <den.bilobram@mail.ru>
2025-04-22 21:28:16 +0300

    leetcode

--- 3/tpo/lab1/app/src/main/java/tpo/task3/Ford.java
+++ 3/tpo/lab1/app/src/main/java/tpo/task3/Ford.java
@@ -1,19 +1,4 @@
 package tpo.task3;
 
-public class Ford extends Character {
-    public Ford(String name) {
-        super(name);
-    }
-
-    @Override
-    public void performAction(Action action) {
-        System.out.println(getName() + " начинает действие: " + action.getDescription());
-        action.execute();
-    }
-
-    @Override
-    public void perceive(SensoryInput input) {
-        System.out.println(getName() + " воспринимает: " + input.getDescription());
-        input.process();
-    }
+public class Model {
 }
